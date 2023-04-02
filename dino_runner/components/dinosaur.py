a102b9414e1081c3d2073d7b73eb4ec0a0a249e3
Francy101001 <126831168+Francy101001@users.noreply.github.com>
2023-04-01 21:55:51 -0500

    Class-1-Assignment

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -1,10 +1,11 @@
 import pygame
 from pygame import Surface
 from pygame.sprite import Sprite
-from dino_runner.utils.constants import RUNNING, JUMPING
+from dino_runner.utils.constants import RUNNING, JUMPING, DUCKING
 
 DINO_JUMPING = "JUMPING"
 DINO_RUNNING = "RUNNING"
+DINO_DUCKING = "DUCKING"
 
 class Dinosaur(Sprite):
     POS_X = 80
@@ -14,8 +15,7 @@ class Dinosaur(Sprite):
     def __init__(self):
         self.image = RUNNING[0]
         self.rect = self.image.get_rect()
-        self.rect.x = self.POS_X
-        self.rect.y = self.POS_Y
+        self.set_position(self.POS_X, self.POS_Y)
         self.step = 0
         self.action = "DINO_RUNNING"
         self.jump_velocity = self.JUMP_VELOCITY
@@ -26,10 +26,14 @@ class Dinosaur(Sprite):
             self.run()
         elif self.action == "DINO_JUMPING":
            self.jump()
+        elif self.action == "DINO_DUCKING":
+            self.duck()
 
-        if self.action != DINO_JUMPING:
+        if self.action != "DINO_JUMPING":
             if user_input[pygame.K_UP]:
                 self.action = "DINO_JUMPING"
+            elif user_input[pygame.K_DOWN]:
+                self.action = "DINO_DUCKING"
             else: 
                 self.action = "DINO_RUNNING"
         
@@ -38,19 +42,27 @@ class Dinosaur(Sprite):
     
     def run(self):
         self.image = RUNNING[self.step // 5]
-        self.rect = self.image.get_rect()
-        self.rect.x = self.POS_X
-        self.rect.y = self.POS_Y
+        self.set_position(self.POS_X, self.POS_Y)
         self.step += 1
     
-    def jump (self):
+    def jump(self):
         self.image = JUMPING
         self.rect.y -= self.jump_velocity * 4
         self.jump_velocity -= 0.8
         if self.jump_velocity < -self.JUMP_VELOCITY:
-            self.action = "DINO_RUNNING"
-            self.rect.y = self.POS_Y
+            self.action = DINO_RUNNING
+            self.set_position(self.POS_X, self.POS_Y)
             self.jump_velocity = self.JUMP_VELOCITY
+    
+    def duck(self):
+        self.image = DUCKING[self.step // 5]
+        self.set_position(self.POS_X, self.POS_Y+40)
+        self.step += 1
+    
+    def set_position(self, x, y):
+        self.rect.x = x
+        self.rect.y = y
+        
 
 
     def draw(self, screen: Surface ):
