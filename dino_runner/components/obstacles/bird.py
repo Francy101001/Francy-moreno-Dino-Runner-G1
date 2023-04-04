e121ae2335cfc957d942f805a2d438ea0474c2f8
Francy101001 <126831168+Francy101001@users.noreply.github.com>
2023-04-04 11:55:37 -0500

    Class3_Assignment

---
+++ dino_runner/components/obstacles/bird.py
@@ -0,0 +1,18 @@
+import random
+from dino_runner.components.obstacles.obstacle import Obstacle
+from dino_runner.utils.constants import BIRD
+
+
+class Bird(Obstacle):
+    def __init__(self):
+        super().__init__(BIRD[0], pos_y=random.choice([280, 220, 170]))
+        self.index = 0
+
+
+        
+    def draw(self, screen):
+        if self.index >= 9:
+            self.index = 0
+     
+        screen.blit(BIRD[self.index // 5], self.rect)
+        self.index += 1
--- dino_runner/components/obstacles/obstacle_manager.py
+++ dino_runner/components/obstacles/obstacle_manager.py
@@ -1,8 +1,11 @@
 
-
+import random
 import pygame
+from dino_runner.components.obstacles.bird import Bird
 from dino_runner.components.obstacles.cactus import Cactus
+from dino_runner.components.obstacles.cactus_small import SmallCactus
 from dino_runner.components.obstacles.obstacle import Obstacle
+
 
 
 class ObstacleManager:
@@ -11,13 +14,20 @@ class ObstacleManager:
 
     def update(self, game):
         if not self.obstacles:
-            self.obstacles.append(Cactus())
+            obstacle_type = random.choice(["cactus", "bird", "small_cactus"])
+            if obstacle_type == "cactus":
+                self.obstacles.append(Cactus())
+            elif obstacle_type == "small_cactus":
+                self.obstacles.append(SmallCactus())
+            else:
+                self.obstacles.append(Bird())
+            
             
         for obstacle in self.obstacles:
             obstacle.update(game.game_speed, self.obstacles)
             if obstacle.rect.colliderect(game.player.rect):
                 pygame.time.delay(500)
-                game.playing = False
+                #game.playing = False
 
     def draw(self, screen):
         for obstacle in self.obstacles:
